e67f0f07c222609944225e2a68a0075249cdd33f
orvalho <rasa396@gmail.com>
2018-01-08 16:49:57 +0200

    #2

--- codewars_solutions.js
+++ codewars_solutions.js
@@ -7,3 +7,22 @@ Create a function that takes an integer as an argument and returns "Even" for ev
 function even_or_odd(number) {
   return number % 2 === 0 ? "Even" : "Odd";
 }
+
+
+
+//2.Opposite number
+
+/*
+Very simple, given a number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+*/
+
+function opposite(number) {
+  return -number;
+}
+
+
